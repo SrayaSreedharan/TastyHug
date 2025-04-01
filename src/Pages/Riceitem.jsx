6237421b6daa85ef14daf6f19871dbe4fb01c317
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-04-01 19:51:12 +0530

    push

--- src/Pages/Riceitem.jsx
+++ src/Pages/Riceitem.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
 import Navbarwe from '../Compoents/Navbarwe'
 import '../Pages/Main.css'
 
 const Riceitem = () => {
+  const[data,setData]=useState([])
+  useEffect(()=>{
+      axios.get(" ").then((response)=>{
+          console.log(response)
+          setData(response.data.data)
+      }).catch((error)=>{
+          console.log(error)
+      })
+
+  },[])
   return (
     <>
    
